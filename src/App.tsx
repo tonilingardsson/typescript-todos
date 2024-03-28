bd12271cde981912f58e17c23e9b3ec4e3d7467e
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-03-28 18:39:31 +0100

    Created Context and 2 components. Rendering.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 
+import { useState } from 'react';
 import './App.css'
+import { AddTodo } from './components/AddTodo';
+import { Todos } from './components/Todos';
+import { ITodoContext, TodoContext } from './contexts/TodoContext'
+import { Todo } from './models/Todo';
 
 function App() {
-  
+  const [state, setState] = useState<ITodoContext>(
+    {
+      todos: [],
+      addTodo: () => {},
+    });
+
+    // We need a starting value for the state.
+    state.addTodo = (text: string) => {
+      setState({ ...state, todos: [...state.todos, new Todo(text)] });
+    };
+
   return (
     <>
-     
+     <TodoContext.Provider value={state}>
+      <AddTodo />
+      <Todos />
+     </TodoContext.Provider>
     </>
-  )
+  );
 }
 
-export default App
+
+export default App;
---
+++ src/contexts/TodoContext.ts
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+import { Todo } from "../models/Todo";
+
+// This is the most "annoying" part of TypeScript:
+// To define the type of the context, we need to use an interface.
+export interface ITodoContext {
+    todos: Todo[];
+    // In these functions we are not returning anything, so we use 'void'.
+    addTodo: (text: string) => void;
+    // deleteTodo: (id: number) => void;
+}
+
+export const TodoContext = createContext<ITodoContext>({
+    todos: [],
+    // addTodo is a function that does nothing yet, that we will use later.
+    addTodo: () => {},
+    // deleteTodo: () => {}
+});
