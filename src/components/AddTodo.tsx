8caf9abf7830bd1fd14789f74d68b9a8a921c911
Toni Lingårdsson Luna <toni.lingardsson@gmail.com>
2024-03-28 19:29:17 +0100

    Form and button to Add Todos.

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,3 +1,22 @@
+import { useContext, useState } from "react";
+import { TodoContext } from "../contexts/TodoContext";
+
 export const AddTodo = () => {
-    return <>AddTodo</>
+    const [text, setText] = useState("");
+// Add funtion that will fetch the data from TodoContext
+    const { addTodo } = useContext(TodoContext);
+
+    return <form onSubmit={(e) => {
+        e.preventDefault();
+        addTodo(text);
+        setText("");
+    }}
+        >
+        <input
+         type="text" 
+         value={text} 
+         onChange={e => setText(e.target.value)}
+        />
+        <button>Save</button>
+    </form>
 }
